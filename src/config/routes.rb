4561f67873303a9b94a11e9e8027cabd54840543
tatsun777 <hakunan12@yahoo.co.jp>
2022-07-18 22:46:25 +0900

    add about page

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
+  get 'top' =>"home#top"
+  get 'about' =>"home#about"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/', to: 'users#index'
 
